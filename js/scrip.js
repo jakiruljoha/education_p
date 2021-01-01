dbdadc0eae4680a41f1c0c164965f1d864285c65
jakiruljoha <73191298+jakiruljoha@users.noreply.github.com>
2021-01-01 19:11:32 +0600

    Add files via upload

---
+++ js/scrip.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+// 	// hide
+// 	$('#hide').click(function(){
+// 		$('h2').fadeOut('fast');
+// 	});
+// 	// show
+// 	$('#show').click(function(){
+// 		$('h2').fadeIn('slow');
+// 	});
+// 	// toggle
+// 	$('#toggle').click(function(){
+// 		$('h2').fadeToggle('400');
+// 	});
+// 	// fadeto
+// 	$('#fadeto').click(function(){
+// 		$('h2').fadeTo(1000, .3);
+// 	});
+// });
